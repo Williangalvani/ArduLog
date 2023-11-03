cd30705210eca162286f1f53731fe497153df791
Willian Galvani <williangalvani@gmail.com>
2023-11-02 23:54:31 -0300

    wip

--- src/components/Displays/Plotly.vue
+++ src/components/Displays/Plotly.vue
@@ -9,14 +9,15 @@
       {{modelValue.name}}
     </v-card-title>
     <v-card-text>
-      <v-img src="https://user-images.githubusercontent.com/6562690/54934415-b4d25b80-4edb-11e9-8758-fb29ada50499.png"></v-img>
+      <div ref="plot" style="width:90%;height:250px;"></div>
       {{modelValue.content}}
     </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
-import { ref, watch, toRefs } from 'vue'
+import { ref, watch, toRefs, onMounted } from 'vue'
+import Plotly from 'plotly.js-dist-min'
 
 export default {
   props: {
@@ -26,25 +27,21 @@ export default {
     }
   },
 
-  data() {
+  
+  setup(props) {
+
+    const plot = ref(null) 
+    onMounted(() => {
+
+    Plotly.plot( plot, [{
+      x: [1, 2, 3, 4, 5],
+      y: [1, 2, 4, 8, 16] }], { 
+        margin: { t: 0 } }, {showSendToCloud:true} );
+        
+      })
     return {
-      count: 0
+      plot
     }
-  },
-
-  // Methods are functions that mutate state and trigger updates.
-  // They can be bound as event handlers in templates.
-  methods: {
-    increment() {
-      this.count++
-    }
-  },
-
-  // Lifecycle hooks are called at different stages
-  // of a component's lifecycle.
-  // This function will be called when the component is mounted.
-  mounted() {
-    console.log(`The initial count is ${this.count}.`)
   }
 }
 </script>
