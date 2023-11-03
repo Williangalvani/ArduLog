2fd12c8e36159567d88e6315ce11df870fc72b8f
Willian Galvani <williangalvani@gmail.com>
2023-11-03 00:18:24 -0300

    wip

--- src/components/Displays/Plotly.vue
+++ src/components/Displays/Plotly.vue
@@ -6,44 +6,40 @@
         end
         icon="mdi-drag"
       ></v-icon>
-      {{modelValue.name}}
     </v-card-title>
     <v-card-text>
       <div ref="plot" style="width:90%;height:250px;"></div>
-      {{modelValue.content}}
     </v-card-text>
   </v-card>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import { ref, watch, toRefs, onMounted } from 'vue'
-import Plotly from 'plotly.js-dist-min'
-
-export default {
-  props: {
-    modelValue: {
-      type: Object,
-      required: true
-    }
-  },
-
-  
-  setup(props) {
+//import Plotly from 'plotly.js-dist-min'
+import Plotly from 'plotly.js'
 
     const plot = ref(null) 
     onMounted(() => {
+      var data = [{
+        x: [1999, 2000, 2001, 2002],
+        y: [10, 15, 13, 17],
+        type: 'scatter'
+      }];
 
-    Plotly.plot( plot, [{
-      x: [1, 2, 3, 4, 5],
-      y: [1, 2, 4, 8, 16] }], { 
-        margin: { t: 0 } }, {showSendToCloud:true} );
-        
+      var layout = {
+        title: 'Sales Growth',
+        xaxis: {
+          title: 'Year',
+          showgrid: false,
+          zeroline: false
+        },
+        yaxis: {
+          title: 'Percent',
+          showline: false
+        }
+      };
+      Plotly.newPlot(plot, data, layout);
       })
-    return {
-      plot
-    }
-  }
-}
 </script>
 
 <style scoped>
--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
       },
     }),
   ],
-  define: { 'process.env': {} },
+  define: { 'process.env': {}, global: {} },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
