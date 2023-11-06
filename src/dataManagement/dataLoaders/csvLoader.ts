c6539069f7dfcc6cb894b41759e922b3af9d00aa
Willian Galvani <williangalvani@gmail.com>
2023-11-05 21:52:50 -0300

    update csv parser

--- src/dataManagement/dataLoaders/csvLoader.ts
+++ src/dataManagement/dataLoaders/csvLoader.ts
@@ -4,46 +4,58 @@ export class CsvLoader extends DataLoader {
 
   public async isDataValid(data: File): Promise<boolean> {
     // Convert the ArrayBuffer to a string
-  
+    let reader = new FileReader();
+    let text = undefined as string | undefined;
 
-    let reader = new FileReader()
-    let text = undefined as string | undefined
-    let timedOut = false
+    // Create a promise to handle FileReader async read
+    const textPromise = new Promise<string | undefined>((resolve, reject) => {
+        reader.onload = () => {
+            if (reader.result) {
+                resolve(new TextDecoder('utf-8').decode((reader.result as ArrayBuffer)));
+            } else {
+                resolve(undefined);
+            }
+        };
+        reader.onerror = () => reject(new Error('File reading failed'));
+        setTimeout(() => reject(new Error('File reading timed out')), 1000);
+    });
 
-    reader.onload = (e) => {
-      console.log(e)
-      if (reader.result) {
-        text = new TextDecoder('utf-8').decode((reader.result.slice(0, 1024)) as ArrayBuffer)
-      }
+    reader.readAsArrayBuffer(data);
+
+    try {
+        text = await textPromise;
+    } catch (error) {
+        console.error(error.message);
+        return false;  // Returning false if there's an error in reading the file or it times out
     }
-    reader.readAsArrayBuffer(data)
-    setInterval(() => {timedOut = true}, 1000)
 
-    while (text === undefined && !timedOut) {
-      await new Promise(r => setTimeout(r, 100));
-    }
-    console.log(text)
+    console.log(text);
     if (text === undefined) {
-      return false
+        return false;
+    }
+
+    // Check for non-ASCII characters
+    if (/[\x80-\xFF]/.test(text)) {
+        console.log('Non-ASCII characters found');
+        return false;  // Non-ASCII characters found
     }
 
     // Check for a valid header (alphabetic characters separated by commas)
-    const headerPattern = /^[a-zA-Z%\s]+(,[a-zA-Z\s]+)*\n/;
-
+    const headerPattern = /^[a-zA-Z%\s]+(,[a-zA-Z\s]+)+\n/;
     if (headerPattern.test(text)) {
-      console.log("header pattern")
-      return true; // Valid header found
+        console.log('header pattern');
+        return true;  // Valid header found
     }
 
-
     // Check for other valid or partial CSV structures
-    if (text.includes(",") || text.includes("\n")) {
-      return true; // It has CSV delimiters (comma or newline)
+    if (text.includes(',') || text.includes('\n')) {
+        return true;  // It has CSV delimiters (comma or newline)
     }
 
     // If neither a valid header nor CSV delimiters are found, it's likely not valid CSV data
     return false;
-  }
+}
+
 
   public supportsStreaming(): boolean {
     return false;
